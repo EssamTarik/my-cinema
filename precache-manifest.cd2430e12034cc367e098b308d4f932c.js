15c98747b630cb83b7f129783b0513beb9397156
EssamTarik <essamtarik2000@gmail.com>
2020-08-15 20:02:32 +0200

    Updates

---
+++ precache-manifest.cd2430e12034cc367e098b308d4f932c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3b9d091d2bad16827109f5fcfb9ff08b",
+    "url": "/my-cinema/index.html"
+  },
+  {
+    "revision": "4cc4de81b756df566ffe",
+    "url": "/my-cinema/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "0cb3d3ce75d366626d21",
+    "url": "/my-cinema/static/css/main.1ff5b2d8.chunk.css"
+  },
+  {
+    "revision": "4cc4de81b756df566ffe",
+    "url": "/my-cinema/static/js/2.4f1e8d8e.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/my-cinema/static/js/2.4f1e8d8e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0cb3d3ce75d366626d21",
+    "url": "/my-cinema/static/js/main.ff7197bd.chunk.js"
+  },
+  {
+    "revision": "e98678023921a04c5e2d",
+    "url": "/my-cinema/static/js/runtime-main.de7d3eef.js"
+  }
+]);
